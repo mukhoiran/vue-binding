4f7424ccccc2b0cfdfd3d95b376d1444b6312208
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-08-09 16:54:02 +0700

    binding style

--- apps.js
+++ apps.js
@@ -2,8 +2,15 @@ var app = new Vue({
   el:'#app',
 
   // <!-- =========== Binding Image =========== -->
+  // data:{
+  //   imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Vue.png/220px-Vue.png',
+  // }
+
+  // <!-- =========== Binding Style =========== -->
   data:{
     imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Vue.png/220px-Vue.png',
+    imagewidth: 100
   }
+
 
 })
