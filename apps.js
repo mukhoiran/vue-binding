8902dd6ece42b4b145dd9d3ebc31105e7f196b60
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-08-09 16:51:05 +0700

    binding image

---
+++ apps.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+  el:'#app',
+
+  // <!-- =========== Binding Image =========== -->
+  data:{
+    imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Vue.png/220px-Vue.png',
+  }
+
+})
